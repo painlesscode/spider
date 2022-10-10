0637797b5b95f4cd9ca5837b801cbc882a783fa6
Hazzaz Bin Faiz <hazzazbinfaiz@gmail.com>
2022-10-10 18:10:58 +0600

    Init Spider

---
+++ src/SpiderServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Painlesscode\Spider;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class SpiderServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+
+    }
+
+    public function boot()
+    {
+
+    }
+}
