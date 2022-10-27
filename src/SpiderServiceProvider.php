21414e110e176f64ee03f40764ca4307ad11da70
Hazzaz Bin Faiz <hazzazbinfaiz@gmail.com>
2022-10-27 12:28:18 +0600

    Use tag injector to inject style

--- src/SpiderServiceProvider.php
+++ src/SpiderServiceProvider.php
@@ -5,6 +5,7 @@ namespace Painlesscode\Spider;
 
 use Illuminate\Routing\ResponseFactory;
 use Illuminate\Support\ServiceProvider;
+use Painlesscode\ModuleConnector\TagInjector;
 use Painlesscode\Spider\Mixin\ResponseMixin;
 
 class SpiderServiceProvider extends ServiceProvider
@@ -17,6 +18,12 @@ class SpiderServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'spider');
+
+        $this->publishes([
+            __DIR__.'/../public' => public_path('vendor/spider'),
+        ], 'public');
+
+        $this->app->make(TagInjector::class)->pushStyle(mix('css/output.css', 'vendor/spider'));
 
         ResponseFactory::mixin(new ResponseMixin());
     }
